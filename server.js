dd1f02d80df6a07bc2050003c96e21f0a725978d
ari000000 <playstation369@outlook.com>
2023-10-14 15:05:25 +0200

    fix1.0

--- server.js
+++ server.js
@@ -1,33 +1,82 @@
-const express = require('express');
-const fs = require('fs').promises;
+const express = require("express");
 const app = express();
-const port = 3000;
+const fs = require("fs");
+const cors = require("cors");
+const port = 8080;
+const filename = __dirname + "/profs.json";
 
-app.use(express.json());
+//Middleware
+app.use(express.json()); //for parsing application/json
+app.use(cors()); //for configuring Cross-Origin Resource Sharing (CORS)
+function log(req, res, next) {
+    console.log(req.method + " Request at" + req.url);
+    next();
+}
+app.use(log);
 
-// POST-Route zum Speichern von Daten in einer Datei
-app.post('/datenSpeichern', async (req, res) => {
-  try {
-    const data = req.body;
-    await fs.writeFile('daten.json', JSON.stringify(data));
-    res.status(201).json({ message: 'Daten erfolgreich gespeichert.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Es ist ein Fehler aufgetreten.' });
-  }
+
+//Endpoints
+app.get("/profs", function (req, res) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+        });
+        res.end(data);
+    });
 });
 
-// GET-Route zum Lesen von gespeicherten Daten aus der Datei
-app.get('/datenLesen', async (req, res) => {
-  try {
-    const data = await fs.readFile('daten.json', 'utf8');
-    res.json(JSON.parse(data));
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Es ist ein Fehler aufgetreten.' });
-  }
+app.get("/profs/:id", function (req, res) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        const dataAsObject = JSON.parse(data)[req.params.id];
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify(dataAsObject));
+    });
 });
 
-app.listen(port, () => {
-  console.log(`Server läuft auf Port ${port}`);
+app.put("/profs/:id", function (req, res) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        let dataAsObject = JSON.parse(data);
+        dataAsObject[req.params.id].name = req.body.name;
+        dataAsObject[req.params.id].rating = req.body.rating;
+        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
+            res.writeHead(200, {
+                "Content-Type": "application/json",
+            });
+            res.end(JSON.stringify(dataAsObject));
+        });
+    });
 });
+
+app.delete("/profs/:id", function (req, res) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        let dataAsObject = JSON.parse(data);
+        dataAsObject.splice(req.params.id, 1);
+        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
+            res.writeHead(200, {
+                "Content-Type": "application/json",
+            });
+            res.end(JSON.stringify(dataAsObject));
+        });
+    });
+});
+
+app.post("/profs", function (req, res) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        let dataAsObject = JSON.parse(data);
+        dataAsObject.push({
+            id: dataAsObject.length,
+            name: req.body.name,
+            rating: req.body.rating,
+        });
+        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
+            res.writeHead(200, {
+                "Content-Type": "application/json",
+            });
+            res.end(JSON.stringify(dataAsObject));
+        });
+    });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
